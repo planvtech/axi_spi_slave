9e39dca468af34ae05b3f8d3a7c20c9bf74358d2
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-08-16 16:18:41 +0200

    replace old cdc fifo with the new one

--- spi_slave_dc_fifo.sv
+++ spi_slave_dc_fifo.sv
@@ -10,47 +10,36 @@
 
 module spi_slave_dc_fifo #(
     parameter DATA_WIDTH = 32,
-      parameter BUFFER_DEPTH = 8
-      )
-      (input logic                  clk_a,
-    input  logic                  rstn_a,
-    input  logic [DATA_WIDTH-1:0] data_a,
-    input  logic                  valid_a,
-    output logic                  ready_a,
-    input  logic                  clk_b,
-    input  logic                  rstn_b,
+    parameter BUFFER_DEPTH = 8
+)(
+    input logic clk_a,
+    input logic rstn_a,
+    input logic [DATA_WIDTH-1:0] data_a,
+    input logic valid_a,
+    output logic ready_a,
+    input logic clk_b,
+    input logic rstn_b,
     output logic [DATA_WIDTH-1:0] data_b,
-    output logic                  valid_b,
-    input  logic                  ready_b
+    output logic valid_b,
+    input logic ready_b
+);
+
+    typedef logic [DATA_WIDTH-1:0] data_t;
+
+    cdc_fifo_gray #(.WIDTH(32), .T(data_t), .LOG_DEPTH(4), .SYNC_STAGES(2)) i_cdc_fifo_gray (
+
+        .src_rst_ni (rstn_a ),
+        .src_clk_i  (clk_a ),
+        .src_data_i (data_a ),
+        .src_valid_i(valid_a),
+        .src_ready_o(ready_a),
+
+        .dst_rst_ni (rstn_b ),
+        .dst_clk_i  (clk_b  ),
+        .dst_data_o (data_b ),
+        .dst_valid_o(valid_b),
+        .dst_ready_i(ready_b)
+
     );
-
-  logic [DATA_WIDTH-1:0] data_async;
-  logic [BUFFER_DEPTH-1:0] write_token;
-  logic [BUFFER_DEPTH-1:0] read_pointer;
-
- dc_token_ring_fifo_din #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .BUFFER_DEPTH(BUFFER_DEPTH)
-    ) u_din (
-    .clk(clk_a),
-    .rstn(rstn_a),
-    .data(data_a),
-    .valid(valid_a),
-    .ready(ready_a),
-    .write_token(write_token),
-    .read_pointer(read_pointer),
-    .data_async(data_async));
-
- dc_token_ring_fifo_dout #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .BUFFER_DEPTH(BUFFER_DEPTH)
-    ) u_dout (.clk(clk_b),
-    .rstn(rstn_b),
-    .data(data_b),
-    .valid(valid_b),
-    .ready(ready_b),
-    .write_token(write_token),
-    .read_pointer(read_pointer),
-    .data_async(data_async));
 
 endmodule
