8bec2ff0f201a625fa16c8e68faae7eafa8753b6
AngelaGonzalezMarino <gonzalezmarinoangela@gmail.com>
2024-02-27 13:16:21 +0100

    correct addr width in ctrl addr

--- axi_spi_slave.sv
+++ axi_spi_slave.sv
@@ -105,7 +105,7 @@ module axi_spi_slave
 
     logic        ctrl_rd_wr;
 
-    logic [AXI_DATA_WIDTH-1:0] ctrl_addr;
+    logic [AXI_ADDR_WIDTH-1:0] ctrl_addr;
     logic        ctrl_addr_valid;
 
     logic [AXI_DATA_WIDTH-1:0] ctrl_data_rx;
